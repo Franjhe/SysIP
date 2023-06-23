4eaff77f26ad53fd39cbc5c1b847e4ba00796d2b
Jhon Alen <jalen@compuamerica.com.ve>
2023-06-23 14:11:23 -0400

    Main Menu - Menu - Sub-Menu

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 import { navItems } from './_nav';
 
@@ -11,5 +15,32 @@ export class DefaultLayoutComponent {
 
   public navItems = navItems;
 
-  constructor() {}
+  constructor( private router: Router,
+               private http: HttpClient) {}
+
+
+  ngOnInit() {
+    console.log('hola')
+    this.getMenus();
+  }
+
+  getMenus() {
+    let params;
+    this.http.post(environment.apiUrl + '/api/v1/menu/get-menu', params).subscribe((response: any) => {
+      const menuPrincipal = response.data.menuPrincipal;
+      const menu = response.data.menu;
+  
+      if (Array.isArray(menuPrincipal) && Array.isArray(menu)) {
+        this.navItems = menuPrincipal.map((item: any, index: number) => ({
+          name: item.xmenu,
+          url: item.xruta,
+          iconComponent: { name: item.xicono },
+          children: menu[index]?.map((childItem: any) => ({
+            name: childItem.xmenu,
+            url: childItem.xruta,
+          })) || [],
+        }));
+      }
+    });  
+  }
 }
