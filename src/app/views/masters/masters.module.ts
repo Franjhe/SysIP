5ae5d91eb379ad93180b005f55e42d8d0d865891
Jhon Alen <jalen@compuamerica.com.ve>
2023-06-26 16:59:48 -0400

    masters

---
+++ src/app/views/masters/masters.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TradesComponent } from './trades/trades.component';
+
+
+
+@NgModule({
+  declarations: [
+    TradesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MastersModule { }
