bff63047351e967560518ed9be80ae657d9a1ce7
Jhon Alen <jalen@compuamerica.com.ve>
2023-08-10 15:28:01 -0400

    busqueda por parques

--- src/app/views/ninja/ninja/ninja.component.ts
+++ src/app/views/ninja/ninja/ninja.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-ninja',
@@ -19,19 +23,27 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   ],
 })
 export class NinjaComponent implements AfterViewInit {
+  search_form!: FormGroup;
+  submitted = false;
   dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
-  columnsToDisplay: string[] = ['cedula', 'nombApell', 'correo', 'nrofac', 'localidad', 'plan_adquirido', 'fecha_in', 'fecha_out'];
+  columnsToDisplay: string[] = ['cedula', 'nombApell', 'correo', 'nrofac', 'hora_fac', 'cantidad_tickes', 'mcosto_ext', 'fingreso'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement: any;
-  columnsName: string[] = ['Cédula', 'Nombre', 'Correo', 'Factura', 'Localidad', 'Plan', 'Fecha In.', 'Fecha Fi.'];
+  columnsName: string[] = ['Cédula', 'Nombre', 'Correo', 'Factura', 'Hora', 'Cantidad T.', 'Costo', 'Fecha'];
   expandedDetailData: any[] = [];
   columnsNameDetail: string[] = ['Nombre Acompañante', 'Item', 'Plan', 'Costo Ext.', 'Costo Local'];
 
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatAutocompleteTrigger) autocompleteTrigger!: MatAutocompleteTrigger;
   showTable: boolean = true;
+  parkList: any[] = [];
+  parkControl = new FormControl('');
+  filteredPark!: Observable<string[]>;
+  loadingData: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private formBuilder: FormBuilder) {}
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
@@ -39,11 +51,55 @@ export class NinjaComponent implements AfterViewInit {
   }
 
   ngOnInit() {
+    this.search_form = this.formBuilder.group({
+      plan_adquirido: ['']
+    });
+
     let data;
+    this.http.post(environment.apiUrl + '/api/v1/valrep/park', data).subscribe((response: any) => {
+      if (response.status) {
+        for(let i = 0; i < response.data.parks.length; i++){
+          this.parkList.push({
+            id: response.data.parks[i].id,
+            value: response.data.parks[i].plan_adquirido
+          })
+        }
+        this.filteredPark = this.parkControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filter(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.parkList
+      .map(park => park.value)
+      .filter(parks => parks.toLowerCase().includes(filterValue));
+  }
+
+  onParkSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedPark = this.parkList.find(park => park.value === selectedValue);
+    if (selectedPark) {
+      this.search_form.get('plan_adquirido')?.setValue(selectedPark.value)
+    }
+    if(this.search_form.get('plan_adquirido')?.value){
+      this.getDataFromPark();
+    }
+  }
+
+  getDataFromPark(){
+    this.loadingData = true;
+    let data = {
+      plan_adquirido: this.search_form.get('plan_adquirido')?.value
+    };
     this.http.post(environment.apiUrl + '/api/v1/ninjaPark/search', data).subscribe((response: any) => {
       if (response.data.list) {
         this.dataSource.data = response.data.list;
       }
+      this.loadingData = false;
     });
   }
 
