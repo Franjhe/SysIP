b333d32fc2b285395481a4cebd4b39e287194198
Jhon Alen <jalen@compuamerica.com.ve>
2023-08-31 10:24:17 -0400

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl : "https://apisys2000.compuamerica.com.ve",
+  // apiUrl : "https://apisys2000.compuamerica.com.ve",
+  apiUrl : "https://localhost:5253",
 };
