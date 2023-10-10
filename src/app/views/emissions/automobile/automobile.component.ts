dc6d05e8b97e26940653ea0924b5c2dc1fd8564a
JhonAlenH <alenjhon9@gmail.com>
2023-10-10 16:17:53 -0400

    Emision de automovil

--- src/app/views/emissions/automobile/automobile.component.ts
+++ src/app/views/emissions/automobile/automobile.component.ts
@@ -15,15 +15,41 @@ export class AutomobileComponent {
   identList = ['V', 'P', 'E', 'J', 'C','G'];
   stateList: any[] = [];
   cityList: any[] = [];
-
+  brandList: any[] = [];
+  modelList: any[] = [];
+  versionList: any[] = [];
+  colorList: any[] = [];
+  ratesList: any[] = [];
+  typeVehicleList: any[] = [];
+  utilityVehicleList:  any[] = [];
+  classList:  any[] = [];
+  planList:  any[] = [];
 
   identControl = new FormControl('');
   stateControl = new FormControl('');
   cityControl = new FormControl('');
+  brandControl = new FormControl('');
+  modelControl = new FormControl('');
+  versionControl = new FormControl('');
+  colorControl = new FormControl('');
+  ratesControl = new FormControl('');
+  typeVehicleControl = new FormControl('');
+  utilityVehicleControl = new FormControl('');
+  classControl = new FormControl('');
+  planControl = new FormControl('');
 
   filteredIdent!: Observable<string[]>;
   filteredState!: Observable<string[]>;
   filteredCity!: Observable<string[]>;
+  filteredBrand!: Observable<string[]>;
+  filteredModel!: Observable<string[]>;
+  filteredVersion!: Observable<string[]>;
+  filteredColor!: Observable<string[]>;
+  filteredRates!: Observable<string[]>;
+  filteredTypeVehicle!: Observable<string[]>;
+  filteredUtilityVehicle!: Observable<string[]>;
+  filteredClass!: Observable<string[]>;
+  filteredPlan!: Observable<string[]>;
 
   personsFormGroup = this._formBuilder.group({
     icedula: ['', Validators.required],
@@ -34,12 +60,26 @@ export class AutomobileComponent {
     email: ['', Validators.required],
     cestado: ['', Validators.required],
     cciudad: ['', Validators.required],
+    xdireccion: ['', Validators.required],
   });
   vehicleFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    xplaca: ['', Validators.required],
+    cmarca: ['', Validators.required],
+    cmodelo: ['', Validators.required],
+    cversion: ['', Validators.required],
+    fano: [{ value: '', disabled: true }],
+    npasajeros: [{ value: '', disabled: true }],
+    ccolor: ['', Validators.required],
+    xserialcarroceria: ['', Validators.required],
+    xserialmotor: ['', Validators.required],
+    xcobertura: ['', Validators.required],
+    ctarifa_exceso: ['', Validators.required],
+    cuso: ['', Validators.required],
+    ctipovehiculo: ['', Validators.required],
+    cclase: ['', Validators.required],
   });
   planFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+    cplan: ['', Validators.required],
   });
   receiptFormGroup = this._formBuilder.group({
     secondCtrl: ['', Validators.required],
@@ -57,6 +97,13 @@ export class AutomobileComponent {
       map(value => this._filter(value || ''))
     );
     this.getState();
+    this.getBrand();
+    this.getColor();
+    this.getRates();
+    this.getTypeVehicles();
+    this.getUtilityVehicle();
+    this.getClass();
+    this.getPlan();
   }
 
   private _filter(value: string): string[] {
@@ -77,13 +124,12 @@ export class AutomobileComponent {
     let data = {
       cpais: 58
     };
-    this.http.post('https://autos.lamundialdeseguros.com/api/valrep/state', data).subscribe((response: any) => {
-      if (response.data.list) {
-        console.log(response.data.list)
+    this.http.post(environment.apiUrl + '/api/v1/valrep/state', data).subscribe((response: any) => {
+      if (response.data.state) {
         for (let i = 0; i < response.data.state.length; i++) {
           this.stateList.push({
             id: response.data.state[i].cestado,
-            value: response.data.state[i].xestado
+            value: response.data.state[i].xdescripcion_l
           });
         }
         this.filteredState = this.stateControl.valueChanges.pipe(
@@ -120,7 +166,7 @@ export class AutomobileComponent {
         for (let i = 0; i < response.data.city.length; i++) {
           this.cityList.push({
             id: response.data.city[i].cciudad,
-            value: response.data.city[i].xciudad
+            value: response.data.city[i].xdescripcion_l
           });
         }
         this.filteredCity = this.cityControl.valueChanges.pipe(
@@ -144,6 +190,313 @@ export class AutomobileComponent {
     if (selectedCity) {
       this.personsFormGroup.get('cestado')?.setValue(selectedCity.id);
       this.getCity();
+    }
+  }
+
+  getBrand(){
+    let data;
+    this.http.post(environment.apiUrl + '/api/v1/valrep/brand', data).subscribe((response: any) => {
+      if (response.data.brand) {
+        for (let i = 0; i < response.data.brand.length; i++) {
+          this.brandList.push({
+            id: response.data.brand[i].cmarca,
+            value: response.data.brand[i].xmarca
+          });
+        }
+        this.filteredBrand = this.brandControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterBrand(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterBrand(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.brandList
+      .map(brand => brand.value)
+      .filter(brand => brand.toLowerCase().includes(filterValue));
+  }
+
+  onBrandSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedBrand = this.brandList.find(brand => brand.value === selectedValue);
+    if (selectedBrand) {
+      this.vehicleFormGroup.get('cmarca')?.setValue(selectedBrand.id);
+      this.getModel();
+    }
+  }
+
+  getModel(){
+    let data = {
+      cmarca: this.vehicleFormGroup.get('cmarca')?.value
+    };
+    this.http.post(environment.apiUrl + '/api/v1/valrep/model', data).subscribe((response: any) => {
+      if (response.data.model) {
+        for (let i = 0; i < response.data.model.length; i++) {
+          this.modelList.push({
+            id: response.data.model[i].cmodelo,
+            value: response.data.model[i].xmodelo
+          });
+        }
+        this.filteredModel = this.modelControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterModel(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterModel(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.modelList
+      .map(model => model.value)
+      .filter(model => model.toLowerCase().includes(filterValue));
+  }
+
+  onModelSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedModel = this.modelList.find(model => model.value === selectedValue);
+    if (selectedModel) {
+      this.vehicleFormGroup.get('cmodelo')?.setValue(selectedModel.id);
+      this.getVersion();
+    }
+  }
+
+  getVersion(){
+    let data = {
+      cmarca: this.vehicleFormGroup.get('cmarca')?.value,
+      cmodelo: this.vehicleFormGroup.get('cmodelo')?.value
+    };
+    this.http.post(environment.apiUrl + '/api/v1/valrep/version', data).subscribe((response: any) => {
+      if (response.data.version) {
+        for (let i = 0; i < response.data.version.length; i++) {
+          this.versionList.push({
+            id: response.data.version[i].cversion,
+            value: response.data.version[i].xversion,
+            npasajeros: response.data.version[i].npasajero,
+            fano: response.data.version[i].cano,
+          });
+        }
+        this.filteredVersion = this.versionControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterVersion(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterVersion(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.versionList
+      .map(version => version.value)
+      .filter(version => version.toLowerCase().includes(filterValue));
+  }
+
+  onVersionSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedVersion = this.versionList.find(version => version.value === selectedValue);
+    if (selectedVersion) {
+      this.vehicleFormGroup.get('cversion')?.setValue(selectedVersion.id);
+      this.changePassager();
+    }
+  }
+
+  changePassager(){
+    let version = this.versionList.find(element => element.id == this.vehicleFormGroup.get('cversion')?.value) ;
+    this.vehicleFormGroup.get('fano')?.setValue(version.fano);
+    this.vehicleFormGroup.get('npasajeros')?.setValue(version.npasajeros);
+  }
+
+  getColor(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/color', null).subscribe((response: any) => {
+      if (response.data.color) {
+        for (let i = 0; i < response.data.color.length; i++) {
+          this.colorList.push({
+            id: response.data.color[i].ccolor,
+            value: response.data.color[i].xcolor,
+          });
+        }
+        this.filteredColor = this.colorControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterColor(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterColor(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.colorList
+      .map(color => color.value)
+      .filter(color => color.toLowerCase().includes(filterValue));
+  }
+
+  onColorSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedColor = this.colorList.find(color => color.value === selectedValue);
+    if (selectedColor) {
+      this.vehicleFormGroup.get('ccolor')?.setValue(selectedColor.id);
+    }
+  }
+
+  getRates(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/rates', null).subscribe((response: any) => {
+      if (response.data.rates) {
+        for (let i = 0; i < response.data.rates.length; i++) {
+          this.ratesList.push({
+            id: response.data.rates[i].ctarifa_exceso,
+            value: response.data.rates[i].xgrupo,
+          });
+        }
+        this.filteredRates = this.ratesControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterRates(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterRates(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.ratesList
+      .map(rates => rates.value)
+      .filter(rates => rates.toLowerCase().includes(filterValue));
+  }
+
+  onRatesSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedRates = this.ratesList.find(rates => rates.value === selectedValue);
+    if (selectedRates) {
+      this.vehicleFormGroup.get('ctarifa_exceso')?.setValue(selectedRates.id);
+    }
+  }
+
+  getTypeVehicles(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/type-vehicle', null).subscribe((response: any) => {
+      if (response.data.type) {
+        for (let i = 0; i < response.data.type.length; i++) {
+          this.typeVehicleList.push({
+            id: response.data.type[i].ctipovehiculo,
+            value: response.data.type[i].xtipovehiculo,
+          });
+        }
+        this.filteredTypeVehicle = this.typeVehicleControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterTypeVehicle(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterTypeVehicle(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.typeVehicleList
+      .map(type => type.value)
+      .filter(type => type.toLowerCase().includes(filterValue));
+  }
+
+  onTypeVehicleSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedTypeVehicle = this.typeVehicleList.find(typeVehicle => typeVehicle.value === selectedValue);
+    if (selectedTypeVehicle) {
+      this.vehicleFormGroup.get('ctipovehiculo')?.setValue(selectedTypeVehicle.id);
+    }
+  }
+
+  getUtilityVehicle(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/utility', null).subscribe((response: any) => {
+      if (response.data.utility) {
+        for (let i = 0; i < response.data.utility.length; i++) {
+          this.utilityVehicleList.push({
+            id: response.data.utility[i].cuso,
+            value: response.data.utility[i].xuso,
+          });
+        }
+        this.filteredUtilityVehicle = this.utilityVehicleControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterUtilityVehicle(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterUtilityVehicle(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.utilityVehicleList
+      .map(utility => utility.value)
+      .filter(utility => utility.toLowerCase().includes(filterValue));
+  }
+
+  onUtilityVehicleSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedUtilityVehicle = this.utilityVehicleList.find(utility => utility.value === selectedValue);
+    if (selectedUtilityVehicle) {
+      this.vehicleFormGroup.get('cuso')?.setValue(selectedUtilityVehicle.id);
+    }
+  }
+
+  getClass(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/class', null).subscribe((response: any) => {
+      if (response.data.class) {
+        for (let i = 0; i < response.data.class.length; i++) {
+          this.classList.push({
+            id: response.data.class[i].cclase,
+            value: response.data.class[i].xclase,
+          });
+        }
+        this.filteredClass = this.classControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterClass(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterClass(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.classList
+      .map(classL => classL.value)
+      .filter(classL => classL.toLowerCase().includes(filterValue));
+  }
+
+  onClassSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedClass = this.classList.find(classL => classL.value === selectedValue);
+    if (selectedClass) {
+      this.vehicleFormGroup.get('cclase')?.setValue(selectedClass.id);
+    }
+  }
+
+  getPlan(){
+    this.http.post(environment.apiUrl + '/api/v1/valrep/plan', null).subscribe((response: any) => {
+      if (response.data.plan) {
+        for (let i = 0; i < response.data.plan.length; i++) {
+          this.planList.push({
+            id: response.data.plan[i].cplan_rc,
+            value: response.data.plan[i].xplan_rc,
+          });
+        }
+        this.filteredPlan = this.planControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterPlan(value || ''))
+        );
+      }
+    });
+  }
+
+  private _filterPlan(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.planList
+      .map(plan => plan.value)
+      .filter(plan => plan.toLowerCase().includes(filterValue));
+  }
+
+  onPlanSelection(event: any) {
+    const selectedValue = event.option.value;
+    const selectedPlan = this.planList.find(plan => plan.value === selectedValue);
+    if (selectedPlan) {
+      this.planFormGroup.get('cplan')?.setValue(selectedPlan.id);
     }
   }
 }
