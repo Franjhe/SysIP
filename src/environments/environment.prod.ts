77103eb2a54968786db95314a23b6532bea0d764
JhonAlenH <alenjhon9@gmail.com>
2023-11-17 08:51:52 -0400

    autos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //  apiUrl : "https://apisys2000.lamundialdeseguros.com",
-  apiUrl : "https://localhost:5253",
+  apiUrl : "https://apisys2000.lamundialdeseguros.com",
+  // apiUrl : "https://localhost:5253",
 };
