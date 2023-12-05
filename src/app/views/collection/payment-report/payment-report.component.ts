dfc68f42e8992ad4df6cb19ff91a7efbdeab4a52
Franjhe <franjhely.andre13@gmail.com>
2023-12-05 11:13:54 -0400

    git add changes

--- src/app/views/collection/payment-report/payment-report.component.ts
+++ src/app/views/collection/payment-report/payment-report.component.ts
@@ -33,15 +33,13 @@ export class PaymentReportComponent {
   backEmitter : any = []
   coinList : any = []
   receiptList : any = []
+  transferList : any = []
   tradesList : any = []
 
   searchReceipt = this._formBuilder.group({
     receipt :  this._formBuilder.array([]),
     transfer : this._formBuilder.array([]),
     xcedula: ['', Validators.required],
-    cmoneda : [''],
-    mpago_dec : [''],
-    ccategoria: [''],
   });
 
   constructor( private _formBuilder: FormBuilder,
@@ -161,7 +159,7 @@ export class PaymentReportComponent {
       ptasaref: '',
       freporte: '',
       xreferencia: '',
-      xruta: '',
+      ximagen: '',
     })
   }
   
@@ -178,13 +176,20 @@ export class PaymentReportComponent {
       cedula: this.searchReceipt.get('xcedula')?.value 
     }
 
-    const creds = this.searchReceipt.get("receipt") as FormArray
+    const receipt = this.searchReceipt.get("receipt") as FormArray
 
-    while (creds.length !== 0) {
-      creds.removeAt(0)
+    while (receipt.length !== 0) {
+      receipt.removeAt(0)
+    }
+
+    const trasnfer = this.searchReceipt.controls.transfer as FormArray;
+
+    while (trasnfer.length !== 0) {
+      trasnfer.removeAt(0)
     }
 
     this.http.post(environment.apiUrl + '/api/v1/collection/search', client ).subscribe((response: any) => {
+
 
       if(response.searchReceipt.receipt.length > 0){
         for(let i = 0; i < response.searchReceipt.receipt.length; i++){
@@ -205,6 +210,7 @@ export class PaymentReportComponent {
           let treatments = this.tradesList
           let filterdata = treatments.filter((data: { id: any; }) => data.id == id)
           const xramo = filterdata[0].value
+
 
           this.receipt.push(
             this._formBuilder.group({
@@ -233,6 +239,7 @@ export class PaymentReportComponent {
 
           this.cliente = response.searchReceipt.client[i].xcliente
         }     
+
         this.addPayment()
 
       }else{
@@ -284,7 +291,10 @@ export class PaymentReportComponent {
         duration: 3000,
         verticalPosition: 'top',
         panelClass: ['error-toast']
-      });  
+      }); 
+      
+      creds.at(i).get('mpago')?.setValue('')
+
     }
 
   }
@@ -294,8 +304,6 @@ export class PaymentReportComponent {
     const creds = this.searchReceipt.get("transfer") as FormArray
 
     const mountDeclare = creds.at(i).get('cmoneda')?.value!
-
-    console.log(mountDeclare)
 
     if(mountDeclare == '$' || mountDeclare == 'EUR' ){
         this.viewBank = false 
@@ -318,28 +326,28 @@ export class PaymentReportComponent {
 
   async llenarlistas(){
 
-    const creds = this.searchReceipt.get("receipt") as FormArray
+    const receipt = this.searchReceipt.get("receipt") as FormArray
 
     this.receiptList = []
 
-    for(let i = 0; i < creds.length; i++){
-      if(creds.value[i].seleccionado == true){
+    for(let i = 0; i < receipt.length; i++){
+      if(receipt.value[i].seleccionado == true){
         this.receiptList.push({
-          cnpoliza: creds.value[i].cnpoliza,
-          cnrecibo: creds.value[i].cnrecibo,
-          crecibo: creds.value[i].crecibo,
-          cpoliza: creds.value[i].cpoliza,
-          fanopol: creds.value[i].fanopol,
-          fmespol: creds.value[i].fmespol,
-          cramo: creds.value[i].cramo,        
-          cmoneda: creds.value[i].cmoneda,
-          fdesde_pol: creds.value[i].fdesde_pol,
-          fhasta_pol: creds.value[i].fhasta_pol,
-          fdesde_rec: creds.value[i].fdesde_rec,
-          fhasta_rec: creds.value[i].fhasta_rec,
-          mprimabruta: creds.value[i].mprimabruta,
-          mprimabrutaext: creds.value[i].mprimabrutaext,
-          ptasamon: creds.value[i].ptasamon,
+          cnpoliza: receipt.value[i].cnpoliza,
+          cnrecibo: receipt.value[i].cnrecibo,
+          crecibo: receipt.value[i].crecibo,
+          cpoliza: receipt.value[i].cpoliza,
+          fanopol: receipt.value[i].fanopol,
+          fmespol: receipt.value[i].fmespol,
+          cramo: receipt.value[i].cramo,        
+          cmoneda: receipt.value[i].cmoneda,
+          fdesde_pol: receipt.value[i].fdesde_pol,
+          fhasta_pol: receipt.value[i].fhasta_pol,
+          fdesde_rec: receipt.value[i].fdesde_rec,
+          fhasta_rec: receipt.value[i].fhasta_rec,
+          mprimabruta: receipt.value[i].mprimabruta,
+          mprimabrutaext: receipt.value[i].mprimabrutaext,
+          ptasamon: receipt.value[i].ptasamon,
 
         });
       }
@@ -350,51 +358,82 @@ export class PaymentReportComponent {
 
   async onSubmit(){
 
-    const formData = new FormData();
-    formData.append('file', this.searchReceipt.get('ximagen')?.value!);
+      this.llenarlistas()
 
-    this.http.post(environment.apiUrl + '/api/upload/image', formData).subscribe((response: any) => {
-        const rutaimage  =  response.uploadedFile.filename
-        if(response.status){
+      const fecha = new Date()
+      const savePaymentTrans = {
+        receipt : this.receiptList,
+        casegurado: this.searchReceipt.get('xcedula')?.value,
+        mpago : this.mountBs,
+        mpagoext : this.mount,
+        ptasamon : this.bcv,
+        freporte : fecha ,
+        cprog : 'Reporte de pago web',
+        cusuario : 13,
+        iestadorec : 'N',
+        ifuente : 'Web Sys2000',
+        ccategoria : this.searchReceipt.get('ccategoria')?.value,
+      }
 
-           this.llenarlistas()
+      //primero llenamos el recipo y la tabla de transacciones 
+      this.http.post(environment.apiUrl + '/api/v1/collection/create-trans',savePaymentTrans).subscribe(async (response: any) => {
 
-          const fecha = new Date()
-          const savePaymentReport = {
-            receipt : this.receiptList,
-            xruta : rutaimage,
-            casegurado: this.searchReceipt.get('xcedula')?.value,
-            cmoneda : this.searchReceipt.get('cmoneda')?.value,
-            mpago_dec : this.searchReceipt.get('mpago_dec')?.value,
-            mpago : this.mountBs,
-            mpagoext : this.mount,
-            ptasamon : this.bcv,
-            freporte : fecha ,
-            cprog : 'Reporte de pago web',
-            cusuario : 13,
-            iestadorec : 'N',
-            ifuente : 'Web Sys2000',
-            ccategoria : this.searchReceipt.get('ccategoria')?.value,
-            xreferencia : this.searchReceipt.get('xreferencia')?.value,
+        const transfer = this.searchReceipt.get("transfer") as FormArray
+
+        //obtenemos el codigo de transaccion 
+        if(response.data.ctransaccion){
+
+          for(let i = 0; i < transfer.length; i++){
+
+            const formData = new FormData();
+            formData.append('file', transfer.at(i).get('ximagen')?.value!);
+        
+            //cargamos las imagenes con el codigo de transaccion
+            this.http.post(environment.apiUrl + '/api/upload/image', formData).subscribe((response: any) => {
+                const rutaimage  =  response.uploadedFile.filename //ruta de imagen por registro 
+
+                this.transferList.push({
+                  cmoneda: transfer.value[i].cmoneda,
+                  cbanco: transfer.value[i].cbanco,
+                  cbanco_destino: transfer.value[i].cbanco_destino,
+                  mpago: transfer.value[i].mpago,
+                  mpagoext: transfer.value[i].mpagoext,
+                  ptasamon: transfer.value[i].ptasamon,
+                  ptasaref: transfer.value[i].ptasaref,        
+                  freporte: transfer.value[i].freporte,
+                  xreferencia: transfer.value[i].xreferencia,
+                  ximagen: rutaimage,
+                });
+
+                const reporData = {
+                  report : this.transferList,
+                  ctransaccion : response.data.ctransaccion
+                }
+
+                if(response.status){
+                  this.http.post(environment.apiUrl + '/api/v1/collection/create-report', reporData).subscribe((response: any) => {
+
+                  })
+
+                }
+            
+              })
+
           }
 
-          this.http.post(environment.apiUrl + '/api/v1/collection/create',savePaymentReport).subscribe(async (response: any) => {
-      
-            this.toast.open(response.message, '', {
-              duration: 5000,
-              verticalPosition: 'top',
-              panelClass: ['success-toast']
-            });  
-
-            await this.toast 
-            location.reload();
-
-          })          
+          this.toast.open(response.message, '', {
+            duration: 5000,
+            verticalPosition: 'top',
+            panelClass: ['success-toast']
+          });  
+  
+          await this.toast 
         }
+  
 
 
-
-    });
+      })          
+        
 
   }
 
