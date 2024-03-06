aff7955bcbd09b9d3dac1bae2a9991cf8ad6bc2c
Gabriel Estacio <gestacio@lamundialdeseguros.com>
2024-03-06 15:53:18 -0400

    al menos lee algo

--- src/app/views/collection/commissions/commissions.component.ts
+++ src/app/views/collection/commissions/commissions.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormGroup, FormControl , FormArray} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort , MatSortModule } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-commissions',
@@ -6,5 +14,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./commissions.component.scss']
 })
 export class CommissionsComponent {
+  groupReceiptsForm = this._formBuilder.group({
+    agrupado : this._formBuilder.array([])
+  });
 
+  constructor( private _formBuilder: FormBuilder,
+    private http: HttpClient,
+    // private sanitizer: DomSanitizer,
+    readonly dialog: MatDialog,
+    private toast: MatSnackBar,
+    private _snackBar: MatSnackBar
+
+    ) {
+   }
+
+   ngOnInit(){
+
+    // fetch(environment.apiUrl +'/api/v1/commissions')
+    // .then((response) => response.json())
+    // .then(data => {
+    //   console.log(data);
+      
+    //   // this.bcv = data.monitors.usd.price
+    // })
+
+    this.http.post(environment.apiUrl + '/api/v1/commissions', '').subscribe((response: any) => {
+      console.log(response);
+      
+      // for(let i = 0; i < response.data.bank.length; i++){
+      //   this.bankInternational.push({
+      //     id: response.data.bank[i].cbanco,
+      //     value: response.data.bank[i].xbanco,
+      //   })        
+      // }
+    })
+
+  }
 }
