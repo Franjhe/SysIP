92cba832a8c2653040bc58027751c2052571ae26
Franjhe <Franjhely.andre13@gmail.com>
2024-03-16 09:50:28 -0400

    add urls prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl : "https://apisys2000.lamundialdeseguros.com",
-  apiUrl_reporte : "",
+  apiUrl : "https://qaapisys2000.lamundialdeseguros.com",
+  apiUrl_reporte : "https://api.lamundialdeseguros.com",
   //  apiUrl : "https://localhost:5253",
 };
