f4c1bf4be4270de9e7ea997c42959bd5e7d702fc
Gabriel Estacio <gestacio@lamundialdeseguros.com>
2024-04-12 15:21:41 -0400

    changes to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl : "https://apisys2000.lamundialdeseguros.com",
+  apiUrl : "https://qaapisys2000.lamundialdeseguros.com",
   apiUrl_reporte: "https://api.lamundialdeseguros.com",
   apiUrl_prod: "https://api.lamundialdeseguros.com",
-  apiUrl: "https://localhost:5253",
+  // apiUrl: "https://localhost:5253",
 };
