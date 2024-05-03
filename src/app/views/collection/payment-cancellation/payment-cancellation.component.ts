cfc4644ba5201f1d8de6eed1c0af39be7b53c1af
Franjhe <franjhely.andre13@gmail.com>
2024-05-03 15:50:34 -0400

    reestructuracion (payment-cancellation)

--- src/app/views/collection/payment-cancellation/payment-cancellation.component.ts
+++ src/app/views/collection/payment-cancellation/payment-cancellation.component.ts
@@ -1,19 +1,10 @@
-import {Component, TemplateRef, ViewChild  } from '@angular/core';
-import {FormBuilder, Validators, FormGroup, FormControl , FormArray} from '@angular/forms';
+import {Component } from '@angular/core';
+import {FormBuilder, FormArray} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort , MatSortModule } from '@angular/material/sort';
-import { MatPaginator } from '@angular/material/paginator';
-import { Observable } from 'rxjs';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
-import * as XLSX from 'xlsx';
-import { saveAs } from 'file-saver';
-import {animate, state, style, transition, trigger} from '@angular/animations';
-
+import {MatDialog } from '@angular/material/dialog';
+import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
 
 @Component({
   selector: 'app-payment-cancellation',
@@ -26,58 +17,12 @@ export class PaymentCancellationComponent {
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource = new MatTableDataSource<any> ;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-
-  @ViewChild('Alerta') InfoReceipt!: TemplateRef<any>;
-  @ViewChild('Alerta1') Alerta1!: TemplateRef<any>;
-
   bcv : any
-  viewData : boolean = false
-  clienteData : any
-
-  urlImg : any
-
-  listCollected : any = []
-  listReceipt: any = []
-
-  dataSoport: any = [] //recibos notificados
-
-  dataReport: any = [] //recibos notificados
-
-  dataReportB: any = [] //recibos notificados
-
-  tradesList : any = []
-  coinList : any = []
-  transferList : any = []
-
-  bankInternational : any = []
-  bankNational: any = []
-
-  bankReceptorInternational : any = []
-  bankReceptorNational : any = []
-  bankReceptorPM : any = []
-  bankReceptorCustodia : any = []
-
-
-  usd : boolean = false
-  pmovil : boolean = false
-  depositoUSD : boolean = false
-  trans: boolean = false
-
-  totalPending : any
   totalNotificated : any
   totalNotificatedExt: any
 
-  messajeError : any
-  error : boolean = false
   revision : boolean = false
   cobradoSAF : boolean = false
-
 
   mount : any //monto de la suma de los recibos 
   mountIGTF : any //monto con el calculo igtf 
@@ -86,33 +31,6 @@ export class PaymentCancellationComponent {
   mountBsP : any //monto en bolivares del porcentaje igtf 
   mountBsExt : any //monto en bolivares del monto total en dolares con igtf
 
-
-  ntransaccion : any //numero de transaccion de recibo notificado
-
-  nrecibo : any //numero de recibo pendiente
-  asegurado : any
-  cliente : any
-  telefono : any
-  correo : any
-  dataReceiptPending : any = []
-  dataReceiptPendingB: any = []
-
-
-  listPending: any = []
-  listVencido : any = []
-  listCollectedReport: any = []
-
-
-  listReceiptClient : any = []
-  lenghReceipt : boolean = false
-
-  lisDiferenceClient: any 
-  listDetalle: any
-  listSoport : any = []
-  diference : boolean = false
-
-  boollistReceipts: boolean = false
-  GroupReceiptsBool : boolean = false
   groupReceiptsForm = this._formBuilder.group({
     agrupado : this._formBuilder.array([])
   });
@@ -125,39 +43,12 @@ export class PaymentCancellationComponent {
     return this.groupReceiptsForm.get("agrupado") as FormArray
   }
 
-  updateReceipt = this._formBuilder.group({
-    iestadorec: [{ value: '', disabled: false }],
-    mdiferencia :[{ value: '', disabled: false }],
-    iestado_tra : [{ value: '', disabled: false }],
-    xobservacion: [{ value: '', disabled: false }],
-    itransaccion: [{ value: '', disabled: false }],
-  });
-
-  updateReceiptPending = this._formBuilder.group({
-    itransaccion :'',
-    cmoneda:'',
-    cbanco:'',
-    cbanco_destino: '',
-    mpago: '',
-    mpagoext: '',
-    ptasamon: '',
-    ptasaref: '',
-    freporte: '',
-    xreferencia: '',
-    ximagen: '',
-    iestadorec: '',
-  });
-
-
-  constructor( private _formBuilder: FormBuilder,
+  constructor( 
+    private _formBuilder: FormBuilder,
     private http: HttpClient,
     private sanitizer: DomSanitizer,
-    readonly dialog: MatDialog,
-    private toast: MatSnackBar,
-    private _snackBar: MatSnackBar
-
-    ) {
-   }
+    readonly dialog: MatDialog
+  ) {}
 
   ngOnInit(){
 
@@ -170,126 +61,6 @@ export class PaymentCancellationComponent {
     .then((response) => response.json())
     .then(data => {
       this.bcv = data.monitors.usd.price
-    })
-
-    fetch(environment.apiUrl + '/api/v1/valrep/trade')
-    .then((response) => response.json())
-    .then(responde => {
-
-      this.tradesList = []
-      for(let i = 0; i < responde.data.trades.length; i++){
-        this.tradesList.push({
-          id: responde.data.trades[i].cramo,
-          value: responde.data.trades[i].xdescripcion_l,
-        })
-      }
-
-    })
-
-    fetch(environment.apiUrl + '/api/v1/valrep/coin')
-    .then((response) => response.json())
-    .then(coin => {
-
-      this.coinList = []
-      for(let i = 0; i < coin.data.coins.length; i++){
-        this.coinList.push({
-          id: coin.data.coins[i].cmoneda,
-          value: coin.data.coins[i].xdescripcion_l,
-        })
-      }
-
-    })
-
-
-    //bancos nacionales transfertencias
-    let bankNational = {
-      ctipopago: 2
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/target-bank', bankNational ).subscribe((response: any) => {
-      for(let i = 0; i < response.data.targetBank.length; i++){
-        this.bankReceptorNational.push({
-          id: response.data.targetBank[i].cbanco_destino,
-          value: response.data.targetBank[i].xbanco,
-        })        
-      }
-
-
-    })
-
-    //bancos internacionales
-    let bankInternational = {
-      ctipopago: 1
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/target-bank', bankInternational ).subscribe((response: any) => {
-      for(let i = 0; i < response.data.targetBank.length; i++){
-        this.bankReceptorInternational.push({
-          id: response.data.targetBank[i].cbanco_destino,
-          value: response.data.targetBank[i].xbanco,
-        })        
-      }
-
-
-    })
-
-    //bancos pago movil
-    let bankReceptorPM = {
-      ctipopago: 3
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/target-bank', bankReceptorPM ).subscribe((response: any) => {
-      for(let i = 0; i < response.data.targetBank.length; i++){
-        this.bankReceptorPM.push({
-          id: response.data.targetBank[i].cbanco_destino,
-          value: response.data.targetBank[i].xbanco,
-        })        
-      }
-
-
-    })
-
-    //bancos custodia
-
-    let bankReceptorCustodia = {
-      ctipopago: 7
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/target-bank', bankReceptorCustodia ).subscribe((response: any) => {
-      for(let i = 0; i < response.data.targetBank.length; i++){
-        this.bankReceptorCustodia.push({
-          id: response.data.targetBank[i].cbanco_destino,
-          value: response.data.targetBank[i].xbanco,
-        })        
-      }
-
-
-    })
-
-    let extranjero = {
-      itipo: 'e'
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/bank', extranjero).subscribe((response: any) => {
-      for(let i = 0; i < response.data.bank.length; i++){
-        this.bankInternational.push({
-          id: response.data.bank[i].cbanco,
-          value: response.data.bank[i].xbanco,
-        })        
-      }
-    })
-
-    let venezolano = {
-      itipo: 'v'
-    }
-
-    this.http.post(environment.apiUrl + '/api/v1/valrep/bank', venezolano).subscribe((response: any) => {
-      for(let i = 0; i < response.data.bank.length; i++){
-        this.bankNational.push({
-          id: response.data.bank[i].cbanco,
-          value: response.data.bank[i].xbanco,
-        })        
-      }
     })
 
 
@@ -305,7 +76,6 @@ export class PaymentCancellationComponent {
 
           const transactionGroup = this._formBuilder.group({
             id: transaction.ctransaccion,
-            ctransaccion: transaction.ctransaccion,
             iestadorec : '',
             xobservacion : '',
             mdiferencia : '',
@@ -414,28 +184,13 @@ export class PaymentCancellationComponent {
 
     })
 
-
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.toLowerCase();
   }
 
   getImage(imageUrl: string): string {
     return environment.apiUrl + '/api/get-document/' + imageUrl;
   }
 
-  Alert(config?: MatDialogConfig) {
-    return this.dialog.open(this.InfoReceipt, config);
-  }
-
-
-  async alerUpdateReceipt(config?: MatDialogConfig) {
-    return this.dialog.open(this.Alerta1, config);
-  }
-
-  updateReceiptNotificated(i : any){
+  onSubmit(i : any){
 
     const creds = this.groupReceiptsForm.controls.agrupado as FormArray;
 
@@ -447,21 +202,20 @@ export class PaymentCancellationComponent {
         let monto = creds.at(i).get('mdiferencia')?.value / this.bcv
         data = {
           transaccion : creds.at(i).get('id')?.value,
-          xobservacion: creds.at(i).get('xobservacion')?.value,
-          mdiferencia: creds.at(i).get('mdiferencia')?.value,
-          mdiferenciaext: monto,
-          iestadorec: creds.at(i).get('iestadorec')?.value,
           casegurado : creds.at(i).get('casegurado')?.value,
           cliente : creds.at(i).get('xcliente')?.value,
-          recibo : creds.at(i).get('recibo')?.value,
           correo : creds.at(i).get('xcorreo')?.value,
-          cmoneda : creds.at(i).get('cmoneda')?.value,
-          idiferencia : creds.at(i).get('idiferencia')?.value,
           tasa : this.bcv,
           cusuario : this.usuario,
           fcobro : new Date(),
-
-
+          reciboConDiferencia: {
+            mdiferencia: creds.at(i).get('mdiferencia')?.value,
+            mdiferenciaext: monto,
+            xobservacion: creds.at(i).get('xobservacion')?.value,
+            idiferencia : creds.at(i).get('idiferencia')?.value,
+            crecibo : creds.at(i).get('recibo')?.value,
+            cmoneda : creds.at(i).get('cmoneda')?.value,
+          }
 
         }
 
@@ -469,20 +223,20 @@ export class PaymentCancellationComponent {
         let monto = creds.at(i).get('mdiferencia')?.value * this.bcv
         data = {
           transaccion : creds.at(i).get('id')?.value,
-          xobservacion: creds.at(i).get('xobservacion')?.value,
-          mdiferenciaext: creds.at(i).get('mdiferencia')?.value,
-          mdiferencia: monto,
           correo : creds.at(i).get('xcorreo')?.value,
-          iestadorec: creds.at(i).get('iestadorec')?.value,
           casegurado : creds.at(i).get('casegurado')?.value,
           cliente : creds.at(i).get('xcliente')?.value,
-          recibo : creds.at(i).get('recibo')?.value,
-          cmoneda : creds.at(i).get('cmoneda')?.value,
-          idiferencia : creds.at(i).get('idiferencia')?.value,
           tasa : this.bcv,
           cusuario : this.usuario,
           fcobro : new Date(),
-
+          reciboConDiferencia: {
+            mdiferencia: monto,
+            mdiferenciaext: creds.at(i).get('mdiferencia')?.value,
+            xobservacion: creds.at(i).get('xobservacion')?.value,
+            idiferencia : creds.at(i).get('idiferencia')?.value,
+            crecibo : creds.at(i).get('recibo')?.value,
+            cmoneda : creds.at(i).get('cmoneda')?.value,
+          }
 
         }
 
@@ -502,15 +256,16 @@ export class PaymentCancellationComponent {
         iestadorec: 'C',
         fcobro : new Date(),
         casegurado : creds.at(i).get('casegurado')?.value,
-        msaldodif: creds.at(i).get('mdiferencia')?.value,
-        cmoneda_dif: creds.at(i).get('cmoneda')?.value,
         cliente : creds.at(i).get('xcliente')?.value,
         correo : creds.at(i).get('xcorreo')?.value,
-        idiferencia : "H",
-        detalle : creds.at(i).get('poliza')?.value,
+        recibo : creds.at(i).get('poliza')?.value,
         ptasamon : this.bcv,
         cusuario : this.usuario,
-
+        balancePositivo:{        
+          cmoneda_dif: creds.at(i).get('cmoneda')?.value,
+          msaldodif: creds.at(i).get('mdiferencia')?.value,
+          idiferencia : "H",
+        }
       }
       this.http.patch(environment.apiUrl + '/api/v1/collection/update-receipt-positive-balance', data ).subscribe((response: any) => {
         if(response.status){
@@ -522,16 +277,13 @@ export class PaymentCancellationComponent {
     else if(creds.at(i).get('iestadorec')?.value == 'C' ){
       const data = {
         transaccion : creds.at(i).get('id')?.value,
-        iestadorec: creds.at(i).get('iestadorec')?.value,
         casegurado : creds.at(i).get('casegurado')?.value,
         correo : creds.at(i).get('xcorreo')?.value,
         cliente : creds.at(i).get('xcliente')?.value,
         fpago : creds.at(i).get('freporte')?.value,
-        detalle : creds.at(i).get('poliza')?.value,
+        recibo : creds.at(i).get('poliza')?.value,
         cusuario : this.usuario,
         fcobro : new Date(),
-
-
       }
       this.http.patch(environment.apiUrl + '/api/v1/collection/update-receipt/', data ).subscribe((response: any) => {
         if(response.status){
@@ -559,12 +311,6 @@ export class PaymentCancellationComponent {
       this.revision = false
 
     }
-
-  }
-
-
-  changeError(){
-    this.error = false 
 
   }
 
